fc4f75b92e9b2fbad286b6c07bf6f37f73422680
1234567890num <53823593+1234567890num@users.noreply.github.com>
2021-09-06 03:55:18 +0800

    Fix crash if filesize is too large

--- Randomize.py
+++ Randomize.py
@@ -2,14 +2,10 @@ import os
 import random
 
 #Pair up Field & Fight theme
-dotk = [0x3C,0x3D,0x3E] #Darkness of the Unknown
-random.shuffle(dotk)
-field = {dotk[0]:dotk[1],
-         dotk[1]:dotk[2],
-         dotk[2]:dotk[0],
-         0x32:0x33, #Dive into the Heart -Destati- & Fragments of Sorrow
+field = {0x32:0x33, #Dive into the Heart -Destati- & Fragments of Sorrow
          0x34:0x35, #The Afternoon Streets & Working Together
          0x36:0x37, #Sacred Moon & Deep Drive
+         0x3C:0x3E, #Darkness of the Unknown I & Darkness of the Unknown III
          0x40:0x41, #What a Surprise?! & Happy Holidays!
          0x44:0x45, #Cavern of Remembrance & Deep Anxiety
          0x64:0x66, #The Underworld & What Lies Beneath
@@ -36,15 +32,13 @@ files = os.listdir(currentDir+'bgm')
 musiclist = []
 for file in files:
     musiclist.append(int(file[5:8]))
-#Remove unwanted tracks
-musiclist.remove(0x84) #Beauty and the Beast (No Loop)
 
 #Shuffle tracks
 randomlist = musiclist.copy()
 random.shuffle(randomlist)
 
 #Ensure Field & Fight tracks are grouped together
-tobereplaced = []
+tobereplaced = [] #Fields to be deleted in arif
 for fightold in fight:
     fieldold = fight[fightold]
     indexfield = musiclist.index(fieldold)
@@ -64,18 +58,38 @@ for fightold in fight:
         indexpaired = randomlist.index(paired)
         randomlist[indexfield],randomlist[indexpaired] = paired, fieldnew
     else:
-        tobereplaced.append([indexfield,fightnew])
-#Replace the field with the fight track
-for i in tobereplaced:
-    randomlist[i[0]] = i[1]
+        tobereplaced.append(fieldold)
 
 #Write the mod.yml
-f = open(currentDir+"mod.yml", "w")
+f = open(currentDir+'mod.yml','w')
 f.write('assets:\n')
 for i in range(len(musiclist)):
     old = musiclist[i]
-    new = randomlist[i]
+    if old in tobereplaced:
+        new = 0x92 #Roxas theme is 2nd lowest filesize
+    else:
+        new = randomlist[i]
     old = 'bgm/music'+str(old).zfill(3)+'.bgm'
     new = 'bgm/music'+str(new).zfill(3)+'.bgm'
     f.write('- name: '+old+'\n  method: copy\n  source:\n  - name: '+new+'\n')
+
+#Replace the field with the fight track
+f.write('- name: 03system.bin\n  method: binarc\n  source:\n')
+f.write('  - name: arif\n    type: list\n    method: copy\n    source:\n')
+f.write('    - name: arif.bin')
 f.close()
+data = open(currentDir+'arifbackup.bin','rb').read()
+for i in range(0x64,0x5865,0x40):
+    for j in range(0,0x10,2):
+        x = data[i+j]
+        if x == 0:
+            continue
+        else:
+            try:
+                data[i+j] = field[x]
+            except:
+                pass
+f = open(currentDir+'arif.bin','wb')
+f.write(data)
+f.close()
+
