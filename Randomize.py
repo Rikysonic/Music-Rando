e651189167e315daecbee2ee93593cb009853cd6
Rikysonic <r.carruezzo@reply.it>
2021-10-23 01:41:11 +0200

    Added KH1 music, reworked script, updated EXE

--- Randomize.py
+++ Randomize.py
@@ -1,11 +1,13 @@
 import os
 import random
 import sys
+from glob import glob
 
-goa_type = 1  # Represents the GoA type, 1 for new rando, 2 for DARKNESS mode, 3 for Relaxed Mermaid mode
+goa_type = 1  # Represents the GoA type, 1 or 2 for new rando, 3 to 6 for meme-y runs
 to_be_replaced = []
 random_list = []
 current_dir = ""
+kh1_mode = False
 
 
 def roll_seed():
@@ -15,8 +17,8 @@ def roll_seed():
     random.shuffle(random_list)
     # Ensure Field & Fight tracks are grouped together
     to_be_replaced = []  # Fields to be deleted in arif
-    # Exit here if DARKNESS or Relaxed Mermaid is selected
-    if goa_type in [2, 3]:
+    # Exit here if DARKNESS, DARKNESS - KH1 Edition, Relaxed Mermaid or Traverse Town is selected
+    if goa_type in range(3, 7):
         return True
     for fight_old in fight:
         field_old = fight[fight_old]
@@ -86,6 +88,15 @@ fight = {}  # Basically reverse of pairs
 for i in field:
     fight[field[i]] = i
 
+# KH1 Field & Fight theme
+kh1_field = {
+    0x17A: 0x17B,  # Blast Away Gummi Ship I & Blast Away Gummi Ship II
+}
+
+kh1_fight = {}  # Basically reverse of pairs
+for i in kh1_field:
+    kh1_fight[kh1_field[i]] = i
+
 # These are the biggest tracks in the game and cannot be assigned to any world music and some GoA custom music
 problematic_tracks = [
     0x6B,  # Ursula's Revenge
@@ -95,7 +106,13 @@ problematic_tracks = [
     0x57,  # Disappeared
     0x3B,  # A Fight to the Death
     0x43,  # Rage Awakened
-    0x6A  # Under the Sea
+    0x6A,  # Under the Sea
+    # KH1 songs
+    0x1C5,  # Night On Bald Mountain
+    # 0x163,  # Disappeared (removed from bgm folder)
+    0x1B9,  # Forze Del Male
+    0x161,  # Disappeared (No Intro)
+    0x1C2  # Guardando Nel Buio (No Intro)
 ]
 
 # Get music ID
@@ -105,26 +122,48 @@ if getattr(sys, 'frozen', False):
 elif __file__:
     current_dir = f"{os.path.dirname(__file__)}"
 
-files = os.listdir(f"{current_dir}/bgm")
+files = glob(f"{current_dir}/bgm/*.bgm")
 music_list = []
 for file in files:
+    file = os.path.basename(file)
     music_list.append(int(file[5:8]))
 
+# Get all the wd files in bgm
+files = glob(f"{current_dir}/bgm/*.wd")
+wd_list = []
+for file in files:
+    wd_list.append(os.path.basename(file))
+
 print("Select the desired option:")
-print("1) Generate new music rando")
-print("2) DARKNESS MODE")
-print("3) Relaxed Mermaid Mode")
-print("4) Exit\n")
+print("1) Generate new music rando (KH2-only tracks)")
+print("2) Generate new music rando (KH2 + KH1 tracks)")
+print("3) DARKNESS MODE")
+print("4) DARKNESS MODE - KH1 Edition")
+print("5) Relaxed Mermaid Mode")
+print("6) I Love Traverse Town!")
+print("7) Exit\n")
 while True:
     try:
         goa_type = int(input("Input number: "))
-        if goa_type in [1, 2, 3]:
+        if goa_type in range(1, 7):
             break
-        if goa_type == 4:
+        elif goa_type == 7:
+            print("Exited!")
             sys.exit(0)
     except ValueError:
         pass
     print("Input not valid.\n")
+
+# Set kh1_mode to True if KH2 + KH1 was selected
+if goa_type == 2:
+    kh1_mode = True
+
+# If kh1_mode, then add the KH1 field/fight pairs to global field/fight dictionary
+if kh1_mode:
+    fight.update(kh1_fight)
+    field.update(kh1_field)
+else:  # Otherwise, remove every KH1 songs from music_list
+    music_list = [x for x in music_list if x <= 200]  # KH2 songs number limit is 190, every greater number is from KH1
 
 valid = False
 while not valid:
@@ -135,15 +174,21 @@ f = open(f"{current_dir}/mod.yml", 'w')
 f.write('assets:\n')
 for i in range(len(music_list)):
     old = music_list[i]
-    if goa_type == 2:
+    if goa_type == 3:
         # Replace EVERY TRACK IN THE GAME with DARKNESS OF THE UNKNOWN I except DARKNESS OF THE UNKNOWN III
         if old == 0x3E:
             new = 0x3E
         else:
             new = 0x3C
-    elif goa_type == 3:
+    elif goa_type == 4:
+        # Replace EVERY TRACK IN THE GAME with Night of Fate from KH1
+        new = 0x183
+    elif goa_type == 5:
         # Replace EVERY TRACK IN THE GAME with Isn't it Lovely?
         new = 0x81
+    elif goa_type == 6:
+        # Replace EVERY TRACK IN THE GAME with Traverse Town
+        new = 0x168
     elif old in to_be_replaced:
         # Set the fight track to be a copy of the corresponding new field track
         field_old = fight[old]
@@ -153,6 +198,12 @@ for i in range(len(music_list)):
         new = random_list[i]
     old = 'bgm/music' + str(old).zfill(3) + '.bgm'
     new = 'bgm/music' + str(new).zfill(3) + '.bgm'
+    f.write('- name: ' + old + '\n  method: copy\n  source:\n  - name: ' + new + '\n')
+
+# Load all the wd files in bgm folder
+for wd in wd_list:
+    old = f'bgm/{wd}'
+    new = f'bgm/{wd}'
     f.write('- name: ' + old + '\n  method: copy\n  source:\n  - name: ' + new + '\n')
 
 # Replace the fight with the field track
@@ -168,9 +219,11 @@ for i in range(0x64, 0x5865, 0x40):
         if x == 0:
             continue
         # If DARKNESS, replace every fight occurrence with the field except the Final Fight
-        # If Relaxed Mermaid, always replace
-        elif (goa_type == 2 and x in fight and x != 0x3E) or (goa_type == 3 and x in fight) or (x in to_be_replaced):
+        # If Relaxed Mermaid, DARKNESS - KH1 Edition or Traverse Town, always replace
+        elif (goa_type == 3 and x in fight and x != 0x3E) or (goa_type in range(4, 7) and x in fight) or \
+                (x in to_be_replaced):
             data[i + j] = fight[x]
 f = open(f"{current_dir}/arif.bin", 'wb')
 f.write(data)
 f.close()
+print("\nFinished! Enjoy your randomized music!")
